4f53fc99061cc6e00ffbcb01e2f0bd9cfe7d726b
Noa Aarts <noa@voorwaarts.nl>
2025-05-25 18:13:50 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+
+    in
+    {
+      devShells = forAllSystems (system: {
+        default = pkgs.${system}.mkShellNoCC {
+          packages = with pkgs.${system}; [
+            (python313.withPackages (
+              ppkgs: with ppkgs; [
+                anyio
+                click
+                idna
+                iniconfig
+                jinja2
+                markdown
+                markupsafe
+                mypy-extensions
+                packaging
+                pathspec
+                platformdirs
+                pluggy
+                pytest
+                python-frontmatter
+                pyyaml
+                ruff
+                sniffio
+              ]
+            ))
+            mask
+          ];
+        };
+
+      });
+      packages = forAllSystems (
+        system:
+        let
+          sps = pkgs.${system};
+        in
+        {
+          alive = sps.python313Packages.alive-progress.overrideAttrs ({
+            postInstall = ''rm $out/LICENSE'';
+          });
+        }
+      );
+    };
+}
