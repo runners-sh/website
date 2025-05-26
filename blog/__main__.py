324c8f361e25854d0392258f21f9c3fa8f5ab43f
peppidesu <bakker.pepijn@gmail.com>
2025-05-26 20:46:31 +0200

    code cleanup

--- blog/__main__.py
+++ blog/__main__.py
@@ -8,7 +8,9 @@ ascii_logo = load_verbatim("ascii/logo.asc")
 ascii_name = load_verbatim("ascii/name.asc")
 page("index.jinja", ascii_logo=ascii_logo, ascii_name=ascii_name)
 
-for dirname, file, name in recurse_files("content", [".md"]):
+for dirname, file, name, _ in recurse_files("content", [".md"]):
 	src_path = path.join(dirname, file)
 	dist_path = path.join(dirname, name + ".html")
 	page_md("blog.jinja", src_path, dist_path)
+
+finalize()
