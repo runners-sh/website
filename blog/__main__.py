1169c444e517008699d84458d3088688f6291449
peppidesu <bakker.pepijn@gmail.com>
2025-05-26 17:39:41 +0200

    fix: files outputting to .jinja instead of html

--- blog/__main__.py
+++ blog/__main__.py
@@ -6,5 +6,5 @@ page("index.jinja")
 
 for dirname, file, name in recurse_files("content", [".md"]):
 	src_path = path.join(dirname, file)
-	dist_path = path.join(dirname, name + ".jinja")
+	dist_path = path.join(dirname, name + ".html")
 	page_md("blog.jinja", src_path, dist_path)
