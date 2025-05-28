8adfc64cce24f9bb0b48f95a41f4ce135e5fd08c
Noa Aarts <noa@voorwaarts.nl>
2025-05-28 17:28:04 +0200

    add tests for italics, bold, links, inline code and strikethrough

--- main-site/e2e/test_gallery.py
+++ main-site/e2e/test_gallery.py
@@ -61,3 +61,44 @@ def test_toc_heading_link_layers(driver):
 	header_link = table_of_contents.find_element(By.LINK_TEXT, "Heading 6")
 	header_link.click()
 	assert driver.current_url == f"{url_post}#heading-6"
+
+
+def test_italics(driver):
+	driver.get(url_post)
+
+	article = driver.find_element(By.TAG_NAME, "article")
+	element = article.find_element(By.XPATH, ".//em")
+	assert element.text == "Italics"
+
+
+def test_bold(driver):
+	driver.get(url_post)
+
+	article = driver.find_element(By.TAG_NAME, "article")
+	element = article.find_element(By.XPATH, ".//strong")
+	assert element.text == "bold"
+
+
+def test_links(driver):
+	driver.get(url_post)
+
+	article = driver.find_element(By.TAG_NAME, "article")
+	element = article.find_element(By.LINK_TEXT, "links")
+	element.click()
+	assert driver.current_url == "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+
+def test_inline_code(driver):
+	driver.get(url_post)
+
+	article = driver.find_element(By.TAG_NAME, "article")
+	element = article.find_element(By.XPATH, ".//code")
+	assert element.text == "inline code"
+
+
+def test_strikethrough(driver):
+	driver.get(url_post)
+
+	article = driver.find_element(By.TAG_NAME, "article")
+	element = article.find_element(By.XPATH, ".//del")
+	assert element.text == "strikethrough"
