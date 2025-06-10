205017045274a8bf6f5414258f95e572383a3cdb
peppidesu <bakker.pepijn@gmail.com>
2025-06-11 00:13:40 +0200

    refactor!: deprecate `with` syntax for Page and MarkdownPage

--- main-site/__main__.py
+++ main-site/__main__.py
@@ -6,7 +6,7 @@ from solstice import *
 ssg = SiteGenerator(output_path="../dist/main-site")
 
 
-def build_blog(ssg) -> list[dict]:
+def build_blog(ssg: SiteGenerator) -> list[dict]:
 	posts = []
 
 	# TODO: this will detect files that collide in main-site but not others
@@ -16,26 +16,31 @@ def build_blog(ssg) -> list[dict]:
 	for dirname, file, name, _ in recurse_files("blog", [".md"]):
 		src_path = path.join(dirname, file)
 		dist_path = path.join(dirname, name + ".html")
-		with MarkdownPage(ssg, "blog.jinja", src_path, dist_path) as pg:
-			if not pg.meta.get("hidden", False):
-				posts.append(pg.meta | {"url": "/" + dist_path.removesuffix(".html")})
 
-			barcode = pg.meta.get("barcode")
-			if barcode is not None:
-				funbar.get_barcode_cache(ssg.output_path).add(barcode)
-				if orig_page := barcode_set.get(barcode):
-					raise Exception(
-						f"duplicate barcode for pages {orig_page} and {src_path}: {barcode}"
-					)
-				barcode_set[barcode] = src_path
-			if barcode is None:
-				warn(f"Barcode not provided for {src_path}, using dummy barcode")
-				barcode = 69
-			pg.set_params(funbar=funbar.html_from_ean8(barcode))
+		pg = MarkdownPage(ssg, "blog.jinja", src_path, dist_path)
+
+		if not pg.meta.get("hidden", False):
+			posts.append(pg.meta | {"url": "/" + dist_path.removesuffix(".html")})
+
+		barcode = pg.meta.get("barcode")
+
+		if barcode is not None:
+			funbar.get_barcode_cache(ssg.output_path).add(barcode)
+			if orig_page := barcode_set.get(barcode):
+				raise Exception(
+					f"duplicate barcode for pages {orig_page} and {src_path}: {barcode}"
+				)
+			barcode_set[barcode] = src_path
+		else:
+			warn(f"Barcode not provided for {src_path}, using dummy barcode")
+			barcode = 69
+
+		pg.set_params(funbar=funbar.html_from_ean8(barcode))
+
+		pg.build()
 
 	posts.sort(key=lambda x: x["date"], reverse=True)
 	ssg.page("blog-overview.jinja", "blog/index.html", posts=posts)
-
 	return posts
 
 
@@ -54,31 +59,33 @@ social_link_icons = {
 }
 
 
-def build_members(ssg, posts) -> list[dict]:
+def build_members(ssg: SiteGenerator, posts) -> list[dict]:
 	members = []
 
 	for dirname, file, name, _ in recurse_files("member", [".md"]):
 		src_path = path.join(dirname, file)
 		dist_path = path.join(dirname, name + ".html")
-		with MarkdownPage(ssg, "member.jinja", src_path, dist_path) as pg:
-			links = [
-				{
-					"type": k,
-					"name": v,
-					"url": social_link_formats[k].format(v),
-					"icon": social_link_icons[k],
-				}
-				for k, v in pg.meta["links"].items()
-			]
-			member_posts = list(filter(lambda x: name in x["authors"], posts))
-			links.sort(key=lambda x: x["name"])
+		pg = MarkdownPage(ssg, "member.jinja", src_path, dist_path)
+		links = [
+			{
+				"type": k,
+				"name": v,
+				"url": social_link_formats[k].format(v),
+				"icon": social_link_icons[k],
+			}
+			for k, v in pg.meta["links"].items()
+		]
+		member_posts = list(filter(lambda x: name in x["authors"], posts))
+		links.sort(key=lambda x: x["name"])
 
-			if not pg.meta.get("pfp"):
-				pg.set_params(pfp=f"/public/pfp/{name}.avif")
+		if not pg.meta.get("pfp"):
+			pg.set_params(pfp=f"/public/pfp/{name}.avif")
 
-			pg.set_params(link_data=links, username=name, posts=member_posts)
+		pg.set_params(link_data=links, username=name, posts=member_posts)
 
-			members.append(pg.meta | {"url": "/" + dist_path.removesuffix(".html")})
+		members.append(pg.meta | {"url": "/" + dist_path.removesuffix(".html")})
+
+		pg.build()
 
 	return members
 
